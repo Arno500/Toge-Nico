52ceb8a326515ee11718656b86900ad74f8edda3
Arno DUBOIS <arno.du@orange.fr>
2019-02-21 09:59:30 +0100

    Importing full Vuesax

--- ressources/src/plugins/vuesax.ts
+++ ressources/src/plugins/vuesax.ts
@@ -1,22 +1,27 @@
 import Vue from "vue";
-import {
-  vsList,
-  vsIcon,
-  vsAvatar,
-  vsInput,
-  vsPopup,
-  vsButton,
-  vsCol,
-  vsRow
-} from "vuesax";
+// import {
+//   vsList,
+//   vsIcon,
+//   vsAvatar,
+//   vsInput,
+//   vsPopup,
+//   vsButton,
+//   vsCol,
+//   vsRow,
+//   vsTooltip
+// } from "vuesax";
+import Vuesax from "vuesax";
+
 import "vuesax/dist/vuesax.css";
 import "material-icons/iconfont/material-icons.css";
 
-Vue.use(vsList);
-Vue.use(vsIcon);
-Vue.use(vsAvatar);
-Vue.use(vsInput);
-Vue.use(vsPopup);
-Vue.use(vsButton);
-Vue.use(vsCol);
-Vue.use(vsRow);
+// Vue.use(vsList);
+// Vue.use(vsIcon);
+// Vue.use(vsAvatar);
+// Vue.use(vsInput);
+// Vue.use(vsPopup);
+// Vue.use(vsButton);
+// Vue.use(vsCol);
+// Vue.use(vsRow);
+// Vue.use(vsTooltip);
+Vue.use(Vuesax);
